8e00e2677d3476143e26703fce7b3e7155f0621d
farazinam <farazinam17@gmail.com>
2025-06-30 19:11:36 +0500

    product CRUD with dropdown complete

--- DISM/PHP/Project/Admin/updateProduct.php
+++ DISM/PHP/Project/Admin/updateProduct.php
@@ -4,52 +4,80 @@ include("connection.php");
 
 $getId = $_GET["id"];
 
-$select = "SELECT * FROM image_crud WHERE product_id = '$getId'";
+$select = "SELECT image_crud.*, category.category_name, brand.brand_name  
+FROM image_crud
+INNER JOIN category
+ON image_crud.category_id = category.category_id
+INNER JOIN brand
+ON image_crud.brand_id = brand.brand_id
+WHERE product_id = '$getId'";
+
 $q1 = mysqli_query($conn, $select);
 
-$dataInArray = mysqli_fetch_assoc($q1)
+$dataInArray = mysqli_fetch_assoc($q1);
+
+$selCat = "SELECT * FROM category";
+$cat_q = mysqli_query($conn, $selCat);
+
+$selBra = "SELECT * FROM brand";
+$brand_q = mysqli_query($conn, $selBra);
+
+
 ?>
 
 <div class="container-fluid pt-4 px-4">
-<div class="row g-4">
-<div class="col-sm-12 col-xl-6">
-                        <div class="bg-light rounded h-100 p-4">
-                            <h6 class="mb-4">Basic Form</h6>
-                            <form method="post" enctype="multipart/form-data">
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Product Name</label>
-                                    <input value="<?php echo $dataInArray['product_name'] ?>" name="pname" type="text" class="form-control" id="exampleInputEmail1"
-                                        aria-describedby="emailHelp">
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Product Price</label>
-                                    <input value="<?php echo  $dataInArray['product_price'] ?>" name="pprice" type="number" class="form-control" id="exampleInputPassword1">
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Product Description</label>
-                                    <input value="<?php echo $dataInArray['product_description'] ?>" name="pdesc" type="text" class="form-control" id="exampleInputEmail1"
-                                        aria-describedby="emailHelp">
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Product Image</label>
-                                    <input name="pimage" type="file" class="form-control" id="exampleInputPassword1">
-                                </div>
-
-                                <!-- <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">category Name</label>
-                                    <input name="cname" type="text" class="form-control" id="exampleInputEmail1"
-                                        aria-describedby="emailHelp">
-                                </div> -->
-
-                                <button name="sub" type="submit" class="btn btn-primary">Add Product</button>
-                            </form>
-                        </div>
+    <div class="row g-4">
+        <div class="col-sm-12 col-xl-6">
+            <div class="bg-light rounded h-100 p-4">
+                <h6 class="mb-4">Basic Form</h6>
+                <form method="post" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Product Name</label>
+                        <input value="<?php echo $dataInArray['product_name'] ?>" name="pname" type="text"
+                            class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
+
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Product Price</label>
+                        <input value="<?php echo  $dataInArray['product_price'] ?>" name="pprice" type="number"
+                            class="form-control" id="exampleInputPassword1">
                     </div>
+
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Product Description</label>
+                        <input value="<?php echo $dataInArray['product_description'] ?>" name="pdesc" type="text"
+                            class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
+
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Product Image</label>
+                        <input name="pimage" type="file" class="form-control" id="exampleInputPassword1">
+                    </div>
+
+                    <div class="mb-3">
+                              <label for="disabledSelect" class="form-label">Select Category</label>
+                              <select name="cid" id="disabledSelect" class="form-select">
+                                  <?php while($cat_data = mysqli_fetch_assoc($cat_q)){ ?>
+                                  <option value="<?php echo $cat_data['category_id'] ?>" <?php if($dataInArray['category_id'] == $cat_data['category_id']){ echo "selected = 'selected'";}?>> <?php echo $cat_data['category_name'] ?> </option>
+                                  <?php } ?>
+                              </select>
+                          </div>
+
+                          <div class="mb-3">
+                              <label for="disabledSelect" class="form-label">Select Category</label>
+                              <select name="bid" id="disabledSelect" class="form-select">
+                                  <?php while($brand_data = mysqli_fetch_assoc($brand_q)){ ?>
+                                  <option value="<?php echo $brand_data['brand_id'] ?>" <?php if($dataInArray['brand_id'] == $brand_data['brand_id']){ echo "selected = 'selected'";}?>> <?php echo $brand_data['brand_name'] ?> </option>
+                                  <?php } ?>
+                              </select>
+                          </div>
+
+                    <button name="sub" type="submit" class="btn btn-primary">Add Product</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php 
 if(isset($_POST["sub"])){
@@ -57,6 +85,8 @@ if(isset($_POST["sub"])){
     $pp = $_POST["pprice"];
     $pd = $_POST["pdesc"];
     $pi = $_FILES["pimage"];
+    $cid = $_POST["cid"];
+    $bid = $_POST["bid"];
 
     $img_name = $pi["name"];
     $img_tmp = $pi["tmp_name"];
@@ -66,7 +96,7 @@ if(isset($_POST["sub"])){
 if(is_uploaded_file($img_tmp)){
 
     $moved = move_uploaded_file($img_tmp, $path);
-    $updated = "UPDATE `image_crud` SET `product_name`='$pn',`product_price`='$pp',`product_description`='$pd', `product_image`='$img_name' WHERE `product_id` = '$getId'";
+    $updated = "UPDATE `image_crud` SET `product_name`='$pn',`product_price`='$pp',`product_description`='$pd', `product_image`='$img_name', `category_id` = '$cid', `brand_id` = '$bid' WHERE `product_id` = '$getId'";
 $done = mysqli_query($conn, $updated);
 
 if($done){
@@ -77,7 +107,7 @@ if($done){
 }
 }
 else{
-    $updated = "UPDATE `image_crud` SET `product_name`='$pn',`product_price`='$pp',`product_description`='$pd' WHERE `product_id` = '$getId'";
+    $updated = "UPDATE `image_crud` SET `product_name`='$pn',`product_price`='$pp',`product_description`='$pd', `category_id` = '$cid', `brand_id` = '$bid' WHERE `product_id` = '$getId'";
 $done = mysqli_query($conn, $updated);
 
 if($done){
@@ -91,5 +121,3 @@ if($done){
 
 include("footer.php");
 ?>
-
-
--- DISM/PHP/Project/Admin/viewProduct.php
+++ DISM/PHP/Project/Admin/viewProduct.php
@@ -1,52 +1,65 @@
 <?php
 include("connection.php");
 
-$sel = "SELECT * FROM image_crud";
+$sel = "SELECT * FROM image_crud 
+INNER JOIN category
+ON image_crud.category_id = category.category_id
+INNER JOIN brand
+ON image_crud.brand_id = brand.brand_id";
 $fet = mysqli_query($conn, $sel);
+
 
 include("header.php");
 ?>
-      <div class="container-fluid pt-4 px-4">
-          <div class="row g-4">
-              <div class="col-sm-12 col-xl-12">
-                  <div class="bg-light rounded h-100 p-4">
-                      <h6 class="mb-4">Product Details</h6>
-                      <table class="table table-striped">
-                          <thead>
-                          <tr class="table-dark">
-            <th>Product Id</th>
-            <th>Product Name</th>
-            <th>Product Price</th>
-            <th>Product Description</th>
-            <th>Product Image</th>
-            <th> Action </th>
-        </tr>
-                          </thead>
-                          <tbody>
-                          <?php       
+<div class="container-fluid pt-4 px-4">
+    <div class="row g-4">
+        <div class="col-sm-12 col-xl-12">
+            <div class="bg-light rounded h-100 p-4">
+                <h6 class="mb-4">Product Details</h6>
+                <table class="table table-striped">
+                    <thead>
+                        <tr class="table-dark">
+                            <th>Product Id</th>
+                            <th>Product Name</th>
+                            <th>Product Price</th>
+                            <th>Product Description</th>
+                            <th>Product Image</th>
+                            <th>category Name</th>
+                            <th>Brand Name</th>
+                            <th> Action </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php       
 while($data = mysqli_fetch_assoc($fet)){
     ?>
 
-        <tr>
-            <td> <?php echo $data["product_id"] ?> </td>
-            <td> <?php echo $data["product_name"] ?> </td>
-            <td> <?php echo $data["product_price"] ?> </td>
-            <td> <?php echo $data["product_description"] ?> </td>
-            <td> <img style="width:100px" src="images/<?php echo $data["product_image"] ?>" alt=""> </td>
-            <td> 
-            <a class="btn btn-warning" href="updateProduct.php?id=<?php echo $data["product_id"] ?>"> Edit </a>  
-            <a class="btn btn-danger" href="deleteProduct.php?id=<?php echo $data["product_id"] ?>" onclick="return confirm('Are You Sure?')"> Delete </a> 
-            </td>
-        </tr>
-<?php } ?>
-                          </tbody>
-                      </table>
-                  </div>
-              </div>
-          </div>
-      </div>
+                        <tr>
+                            <td> <?php echo $data["product_id"] ?> </td>
+                            <td> <?php echo $data["product_name"] ?> </td>
+                            <td> <?php echo $data["product_price"] ?> </td>
+                            <td> <?php echo $data["product_description"] ?> </td>
+                            <td> <img style="width:100px" src="images/<?php echo $data["product_image"] ?>" alt="">
+                            </td>
+                            <td> <?php echo $data["category_name"] ?> </td>
+                            <td> <?php echo $data["brand_name"] ?> </td>
+                            <td>
+                                <a class="btn btn-warning"
+                                    href="updateProduct.php?id=<?php echo $data["product_id"] ?>"> Edit </a>
+                                <a class="btn btn-danger" href="deleteProduct.php?id=<?php echo $data["product_id"] ?>"
+                                    onclick="return confirm('Are You Sure?')"> Delete </a>
+                            </td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 
-      <?php
+<?php
 
 include("footer.php");
 ?>
+
